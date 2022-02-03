e25b19cbcf79b551583ee16fe644aca388810c24
Denzel <dbartolaba123@gmail.com>
2022-02-02 17:29:16 -0800

    Ci test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Name', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/name/i);
   expect(linkElement).toBeInTheDocument();
 });
